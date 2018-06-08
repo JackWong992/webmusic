4291ed779290ddf82f50709e218f9ce5c3d48da9
JackWong992 <jackwong992@163.com>
2018-06-09 07:27:51 +0800

    update

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ function $(selector){
 //封装音乐列表
 function getMusicList(callback){
   var xhr = new XMLHttpRequest()
-  xhr.open('GET', '/music.json', true)
+  xhr.open('GET', 'https://www.easy-mock.com/mock/5b1b0fd7496c9b36644e63dc/index/index', true)
   xhr.onload = function(){
     if((xhr.status >=200 && xhr.status < 300) || xhr.status === 304){
       callback(JSON.parse(this.responseText))
